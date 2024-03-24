254b8cb9259b1fe4086da5503c2fd4725837c344
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2024-03-24 04:22:45 +0100

    Add data class

---
+++ app/src/main/java/ba/unsa/etf/rma/cuvarkuca/Biljka.kt
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rma.cuvarkuca
+
+data class Biljka (
+    val naziv: String,
+    val porodica: String,
+    val medicinskoUpozorenje: String,
+    val medicinskeKoristi: List<MedicinskaKorist>,
+    val profilOkusa: ProfilOkusaBiljke,
+    val jela: List<String>,
+    val klimatskiTipovi: List<KlimatskiTip>,
+    val zemljisniTipovi: List<Zemljiste>
+)
