079acb4a6c8322b0cc1dd065dec4160d6780477f
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2024-03-24 04:22:22 +0100

    Add enum classes

---
+++ app/src/main/java/ba/unsa/etf/rma/cuvarkuca/Zemljiste.kt
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rma.cuvarkuca
+
+enum class Zemljiste (val naziv: String) {
+    PJESKOVITO("Pjeskovito zemljište"),
+    GLINENO("Glineno zemljište"),
+    ILOVACA("Ilovača"),
+    CRNICA("Crnica"),
+    SLJUNOVITO("Šljunovito zemljište"),
+    KRECNJACKO("Krečnjačko zemljište")
+}
