675955d6b5cf8397a8eeb592716f4bb9b50aa117
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2024-04-28 23:50:35 +0200

    AddNewPlant test

--- app/src/androidTest/java/ba/unsa/etf/rma/cuvarkuca/ValidationInstrumentedTest.kt
+++ app/src/androidTest/java/ba/unsa/etf/rma/cuvarkuca/ValidationInstrumentedTest.kt
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rma.cuvarkuca
 
 import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
@@ -10,11 +11,16 @@ import androidx.test.espresso.action.ViewActions.pressImeActionButton
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
 import org.hamcrest.Matchers.hasToString
 import org.hamcrest.Matchers.not
 import org.junit.Rule
@@ -86,5 +92,75 @@ class ValidationInstrumentedTest {
         onData(not(hasToString("AppLe")))
             .inAdapterView(withId(R.id.jelaLV))
             .perform(click())
+    }
+
+    @Test
+    fun addNewPlant() {
+        onView(withId(R.id.novaBiljkaBtn)).perform(click())
+
+        onView(withId(R.id.nazivET))
+            .perform(scrollTo())
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Name"), pressImeActionButton())
+        closeSoftKeyboard()
+
+        onView(withId(R.id.porodicaET))
+            .perform(scrollTo())
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Family"), pressImeActionButton())
+        closeSoftKeyboard()
+
+        onView(withId(R.id.medicinskoUpozorenjeET))
+            .perform(scrollTo())
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Warning"), pressImeActionButton())
+        closeSoftKeyboard()
+
+        onView(withId(R.id.jeloET))
+            .perform(scrollTo())
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Meal"), pressImeActionButton())
+        closeSoftKeyboard()
+
+        onView(withId(R.id.dodajJeloBtn)).perform(scrollTo()).perform(click())
+
+        onView(withId(R.id.medicinskaKoristLV)).perform(scrollTo()).perform(click())
+        onData(anything())
+            .inAdapterView(withId(R.id.medicinskaKoristLV))
+            .atPosition(2)
+            .perform(click())
+
+        onView(withId(R.id.profilOkusaLV)).perform(scrollTo()).perform(click())
+        onData(anything())
+            .inAdapterView(withId(R.id.profilOkusaLV))
+            .atPosition(3)
+            .perform(click())
+
+        onView(withId(R.id.klimatskiTipLV)).perform(scrollTo()).perform(click())
+        onData(anything())
+            .inAdapterView(withId(R.id.klimatskiTipLV))
+            .atPosition(1)
+            .perform(click())
+
+        onView(withId(R.id.zemljisniTipLV)).perform(scrollTo()).perform(click())
+        onData(anything())
+            .inAdapterView(withId(R.id.zemljisniTipLV))
+            .atPosition(4)
+            .perform(click())
+
+        onView(withId(R.id.dodajBiljkuBtn)).perform(scrollTo()).perform(click())
+
+        onView(withId(R.id.biljkeRV)).perform(
+            RecyclerViewActions.scrollTo<RecyclerView.ViewHolder>(
+                allOf(
+                    hasDescendant(withText("Name")),
+                    hasDescendant(withId(R.id.nazivItem))
+                )
+            )
+        )
     }
 }
