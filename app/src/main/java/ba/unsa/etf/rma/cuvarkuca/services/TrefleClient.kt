24ee7a326d1e71a540d2a5235a88648050a9369e
Kanita Kadusic <kkadusic2@etf.unsa.ba>
2024-06-17 01:33:45 +0200

    Switch to http (API error)

--- app/src/main/java/ba/unsa/etf/rma/cuvarkuca/services/TrefleClient.kt
+++ app/src/main/java/ba/unsa/etf/rma/cuvarkuca/services/TrefleClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object TrefleClient {
     val retrofit: TrefleService = Retrofit.Builder()
-        .baseUrl("https://trefle.io/api/v1/")
+        .baseUrl("http://trefle.io/api/v1/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(TrefleService::class.java)
